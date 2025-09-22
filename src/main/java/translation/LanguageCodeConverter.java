460381651b1eecc8650bbd710b2510b0e67e05c3
Rasyid Rafi Pamuji <thenameis.rasyid@gmail.com>
2025-09-22 14:53:31 -0400

    Completed Task A, passed tests
    Max's computer is broken and helped here

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,12 +42,18 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                // TODO Task A: use line to populate the instance variables
+                String[] tokens = line.split("\\s+");
+                int right = tokens.length - 1;
+                languageCodeToLanguage.put(tokens[right], tokens[0]);
+                for (int i = 0; i < right; i++) {
+                    languageToLanguageCode.put(tokens[i], tokens[right]);
+                }
             }
 
         } catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
+
     }
 
     /**
@@ -56,8 +62,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return code;
+        return languageCodeToLanguage.get(code);
     }
 
     /**
@@ -65,9 +70,9 @@ public class LanguageCodeConverter {
      * @param language the name of the language
      * @return the 2-letter code of the language
      */
+
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
-        return language;
+        return languageToLanguageCode.get(language);
     }
 
     /**
